e40145333d2ba9db8496edd4f9439abbf30f120b
Christopher Chase <christopher.a.chase@outlook.com>
2022-08-22 08:02:39 -0500

    Created Component files for each part of the table

---
+++ Application/src/components/Table/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => (
+    <thead>
+        <th>Quest Name</th>
+        <th>Skill Progress</th>
+        {/* <th>Quest Progress</th> */}
+    </thead>
+);
+
+export default Header;
---
+++ Application/src/components/Table/Table.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./Header";
+
+const Table = (tableData) => (
+    <table>
+        <Header/>
+    </table>
+);
+
+export default Table;
