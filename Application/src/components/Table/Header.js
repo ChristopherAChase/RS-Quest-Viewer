b5d81d69d03fa2bfca60573ce7462a4a89890d00
Christopher Chase <Christopher.A.Chase@outlook.com>
2022-08-22 13:06:25 -0500

    Updated Header component with the column names that will be displayed

--- Application/src/components/Table/Header.js
+++ Application/src/components/Table/Header.js
@@ -2,9 +2,13 @@ import React from "react";
 
 const Header = () => (
     <thead>
-        <th>Quest Name</th>
-        <th>Skill Progress</th>
-        {/* <th>Quest Progress</th> */}
+        <tr>
+            <th>Quest Name</th>
+            <th>Difficulty</th>
+            <th>Length</th>
+            <th>Quest Points</th>
+            <th>Is Members?</th>
+        </tr>
     </thead>
 );
 
