8d2b23d0621432aea15c93ddd3993ac088169bad
Christopher Chase <christopher.a.chase@outlook.com>
2022-08-23 08:01:17 -0500

    Update rendering in index.js to use the new createRoot function

--- Application/src/index.js
+++ Application/src/index.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import App from './App';
+const root = ReactDOM.createRoot(document.getElementById('app'));
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('app')
-);
+root.render(<App/>);
