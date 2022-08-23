149013b2ea8afc1ce404c15e6f3fd86862610464
Christopher Chase <christopher.a.chase@outlook.com>
2022-08-23 07:59:21 -0500

    Update file names to jsx files and updated format to follow best practices

---
+++ Application/src/components/Table/TableHeader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TableHeader = () => (
+  <thead>
+    <tr>
+      <th>Quest Name</th>
+      <th>Difficulty</th>
+      <th>Length</th>
+      <th>Quest Points</th>
+      <th>Is Members?</th>
+    </tr>
+  </thead>
+);
+
+export default TableHeader;
