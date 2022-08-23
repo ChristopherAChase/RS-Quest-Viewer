9b8bb430827969c07048316c5297785585809efd
Christopher Chase <Christopher.A.Chase@outlook.com>
2022-08-23 14:14:32 -0500

    Added new constants to use

--- Scraping/utils/constants.py
+++ Scraping/utils/constants.py
@@ -1,14 +1,15 @@
 ALL_SKILLS = [
-    'Agility'     , 'Archaeology'   , 'Attack'   , 'Constitution', 
-    'Construction', 'Cooking'       , 'Crafting' , 'Defence', 
-    'Divination'  , 'Dungeoneering' , 'Farming'  , 'Firemaking', 
-    'Fishing'     , 'Fletching'     , 'Herblore' , 'Hunter', 
-    'Invention'   , 'Magic'         , 'Mining'   , 'Prayer', 
-    'Ranged'      , 'Runecrafting'  , 'Slayer'   , 'Smithing', 
-    'Strength'    , 'Summoning'     , 'Thieving' , 'Woodcutting'
+    'Agility', 'Archaeology', 'Attack', 'Constitution',
+    'Construction', 'Cooking', 'Crafting', 'Defence',
+    'Divination', 'Dungeoneering', 'Farming', 'Firemaking',
+    'Fishing', 'Fletching', 'Herblore', 'Hunter',
+    'Invention', 'Magic', 'Mining', 'Prayer',
+    'Ranged', 'Runecrafting', 'Slayer', 'Smithing',
+    'Strength', 'Summoning', 'Thieving', 'Woodcutting'
 ]
 
 BASE_WIKI_LINK = r'https://runescape.wiki'
 SKILL_LINK_SUFFIX = r'/w/{skill}'
 QUEST_LIST_LINK_SUFFIX = r'/w/List_of_quests'
 QUEST_REQUIREMENTS_HTML_ID = r'Quests_with_{article}_{skill}_requirement'
+QUEST_LIST_URL = f'{BASE_WIKI_LINK}{QUEST_LIST_LINK_SUFFIX}'
