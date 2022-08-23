5f9cb28dd731f9838f7418029153ff88626ba90d
Christopher Chase <Christopher.A.Chase@outlook.com>
2022-08-23 14:17:57 -0500

    Remove conditional addition of skillRequirements array as it's being added in the quests.py script

--- Scraping/skills.py
+++ Scraping/skills.py
@@ -1,15 +1,13 @@
 import re
 from utils.constants import ALL_SKILLS
-from utils.utility import *
+from utils.utility import get_skill_html_id, get_skill_wiki_url, titleCase
 import requests
 from bs4 import BeautifulSoup
 
-SKILL_REQS = "Skill_Requirements"
-
-def get_quests_requiring_skill(skill:str):
+def get_quests_requiring_skill(skill: str):
     skill_html_id = get_skill_html_id(skill)
     skill_link = get_skill_wiki_url(skill)
-    
+
     soup = BeautifulSoup(requests.get(skill_link).content, "html.parser")
 
     quest_requirements_list_items = soup.find("span", "mw-headline", string=skill_html_id.replace('_', ' ')) \
@@ -18,23 +16,22 @@ def get_quests_requiring_skill(skill:str):
 
     return [requirement.text for requirement in quest_requirements_list_items]
 
-def get_quest_requirement(requirement:str):
+
+def get_quest_requirement(requirement: str):
     stringSplitIndices = [0, re.search('[^0-9]', requirement).start(), re.search('[^0-9- ]', requirement).start()]
-    return [requirement[i:j] for i,j in zip(stringSplitIndices, stringSplitIndices[1:]+[None])][::2]
+    return [requirement[i:j] for i, j in zip(stringSplitIndices, stringSplitIndices[1:] + [None])][::2]
+
 
 def add_skill_requirements_to_quest_dictionary(quest_dictionary: dict):
     quest_list = quest_dictionary
     for skill in ALL_SKILLS:
         for requirement in get_quests_requiring_skill(skill):
-            skill_level, quest_name =  get_quest_requirement(requirement)
+            skill_level, quest_name = get_quest_requirement(requirement)
 
-            quest = next((item for item in quest_list["Quests"] if item["Name"] == quest_name.title()), None)
+            quest = next((item for item in quest_list["Quests"] if item["name"] == titleCase(quest_name)), None)
             if quest:
-                if not SKILL_REQS in quest:
-                    quest[SKILL_REQS] = []
-
-                quest[SKILL_REQS].append({
-                        "Skill": skill,
-                        "Level": int(skill_level)
-                    })
+                quest["skillRequirements"].append({
+                    "skillName": skill,
+                    "requiredLevel": int(skill_level)
+                })
     return quest_list
