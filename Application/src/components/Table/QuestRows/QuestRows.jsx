0dbcf079c578f1a3e3daaf8136be1a0df4230c95
Christopher Chase <Christopher.A.Chase@outlook.com>
2022-09-15 08:41:32 -0500

    Restructured rows to be quest rows parent component
    Moved each row that was inside the 'Row' component into their own component

---
+++ Application/src/components/Table/QuestRows/QuestRows.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import QuestOverviewRow from "./QuestOverviewRow";
+import RequirementsRow from "./RequirementsRow/RequirementsRow";
+
+const QuestRows = ({ rowData, onToggle, expanded, ...props }) => {
+  const {
+    skillRequirements,
+    questRequirements
+  } = rowData;
+
+  return (
+    <>
+      <QuestOverviewRow
+        rowData={rowData}
+        onToggle={onToggle}
+        expanded={expanded}
+      />
+      <RequirementsRow
+        questRequirements={questRequirements}
+        skillRequirements={skillRequirements}
+        expanded={expanded}
+      />
+    </>
+  );
+};
+
+export default QuestRows;
